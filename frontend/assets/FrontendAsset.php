cfa5e17484a136438b8bc0d30d443e2129ea258d
Alfredo Sotil <alfredo.sotil@mambo.pe>
2018-02-15 18:40:10 -0500

    alfredosotil

--- frontend/assets/FrontendAsset.php
+++ frontend/assets/FrontendAsset.php
@@ -30,16 +30,39 @@ class FrontendAsset extends AssetBundle
      * @var array
      */
     public $css = [
-        'bundle/style.css',
-//        'css/style.css',
+//        'bundle/style.css',
+        'css/css-plugin-collections.css',
+        'css/menuzord-skins/menuzord-boxed.css',
+        'css/style-main.css',
+        'css/colors/theme-skin-lemon.css',
+        'css/custom-bootstrap-margin-padding.css',
+        'css/responsive.css',
+        'js/revolution-slider/css/settings.css',
+        'js/revolution-slider/css/layers.css',
+        'js/revolution-slider/css/navigation.css',
     ];
 
     /**
      * @var array
      */
     public $js = [
-        'bundle/app.js',
-//        'js/app.js',
+//        'bundle/app.js',
+        'js/jquery-2.2.4.min.js',
+        'js/jquery-ui.min.js',
+        'js/bootstrap.min.js',
+        'js/jquery-plugin-collection.js',
+        'js/revolution-slider/js/jquery.themepunch.revolution.min.js',
+        'js/calendar-events-data.js',
+        'js/custom.js',
+        'js/revolution-slider/js/extensions/revolution.extension.actions.min.js',
+        'js/revolution-slider/js/extensions/revolution.extension.carousel.min.js',
+        'js/revolution-slider/js/extensions/revolution.extension.kenburn.min.js',
+        'js/revolution-slider/js/extensions/revolution.extension.layeranimation.min.js',
+        'js/revolution-slider/js/extensions/revolution.extension.migration.min.js',
+        'js/revolution-slider/js/extensions/revolution.extension.navigation.min.js',
+        'js/revolution-slider/js/extensions/revolution.extension.parallax.min.js',
+        'js/revolution-slider/js/extensions/revolution.extension.slideanims.min.js',
+        'js/revolution-slider/js/extensions/revolution.extension.video.min.js',
     ];
 
     /**
--- frontend/views/layouts/_clear.php
+++ frontend/views/layouts/_clear.php
@@ -21,6 +21,68 @@ use yii\helpers\Html;
         <?php echo Html::csrfMetaTags() ?>
     </head>
     <body>
+        <header id="header" class="header">
+            <div class="header-top sm-text-center bg-theme-colored">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-4">
+                            <nav>
+                                <ul class="list-inline sm-text-center text-left flip mt-5">
+                                    <li> <a class="text-white" href="#">FAQ</a> </li>
+                                    <li class="text-white">|</li>
+                                    <li> <a class="text-white" href="#">Help Desk</a> </li>
+                                    <li class="text-white">|</li>
+                                    <li> <a class="text-white" href="#">Support</a> </li>
+                                </ul>
+                            </nav>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="widget m-0 mt-5 no-border">
+                                <ul class="list-inline text-right sm-text-center">
+                                    <li class="pl-10 pr-10 mb-0 pb-0">
+                                        <div class="header-widget text-white"><i class="fa fa-phone"></i> 123-456-789 </div>
+                                    </li>
+                                    <li class="pl-10 pr-10 mb-0 pb-0">
+                                        <div class="header-widget text-white"><i class="fa fa-envelope-o"></i> contact@yourdomain.com </div>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="col-md-2 text-right flip sm-text-center">
+                            <div class="widget m-0">
+                                <ul class="styled-icons icon-dark icon-circled icon-theme-colored icon-sm">
+                                    <li class="mb-0 pb-0"><a href="#"><i class="fa fa-facebook"></i></a></li>
+                                    <li class="mb-0 pb-0"><a href="#"><i class="fa fa-twitter"></i></a></li>
+                                    <li class="mb-0 pb-0"><a href="#"><i class="fa fa-instagram"></i></a></li>
+                                    <li class="mb-0 pb-0"><a href="#"><i class="fa fa-linkedin text-white"></i></a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="header-nav">
+                <div class="header-nav-wrapper bg-light navbar-scrolltofixed">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-md-12">
+                                <div id="menuzord-right" class="menuzord orange no-bg"> <a class="menuzord-brand stylish-header pull-left flip" href="javascript:void(0)"><img src="<?= Yii::$app->request->baseUrl; ?>/images/logo-wide-white.png" alt=""></a>
+                                    <ul class="menuzord-menu onepage-nav">
+                                        <li class="active"><a href="#home">Inicio</a></li>
+                                        <li><a href="#about">TBJJP</a></li>
+                                        <li><a href="#services">Eventos</a></li>
+                                        <li><a href="#trainer">Arbitros</a></li>
+                                        <li><a href="#pricing">Inscripciones</a></li>
+                                        <li><a href="#blog">Auspicios</a></li>
+                                        <li><a href="#contact">Contact</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
         <?php $this->beginBody() ?>
         <?php echo $content ?>
         <?php $this->endBody() ?>
